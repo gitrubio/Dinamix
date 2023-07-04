9de5b764441898bd7450659aaec5a77dfc9b2094
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-07-04 14:44:03 -0500

    add : config proyect

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-	const [count, setCount] = useState(0)
-
+  
 	return (
-		<>
-			<div>
-				<a href='https://vitejs.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount(count => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		<Routes>
+			30 <Route path='/' element={<p>asdasdas</p>} />
+			31 <Route path='/auth' element={<>asa</>} />
+			31 <Route path='*' element={<p> Not Found</p>} />
+		</Routes>	
 	)
 }
 
