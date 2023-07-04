d1f177ecb1ac77f346891b3ee08dd3a822540ef9
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-07-04 15:17:16 -0500

    add : cambios xd

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { PublicRoutes } from './models/routes'
+import Login from './components/Login'
 
 function App() {
-  
 	return (
-		<Routes>
-			30 <Route path='/' element={<p>asdasdas</p>} />
-			31 <Route path='/auth' element={<>asa</>} />
-			31 <Route path='*' element={<p> Not Found</p>} />
-		</Routes>	
+		<BrowserRouter>
+			<Routes>
+				30 <Route path='/' element={<p>asdasdas</p>} />
+				31 <Route path={PublicRoutes.LOGIN} element={<Login/>} />
+				31 <Route path='*' element={<p> Not Found</p>} />
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
