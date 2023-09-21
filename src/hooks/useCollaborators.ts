fc45151d74e511956da81acde4bd3791e8ce0414
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-09-21 09:28:56 -0500

    add : prettier change

--- src/hooks/useCollaborators.ts
+++ src/hooks/useCollaborators.ts
@@ -1,45 +1,64 @@
 import React, { useState, useEffect } from 'react'
 import { CollaboratorServices } from '../services'
-import { Collaborator } from '../interfaces/collaborators.interface';
+import { Collaborator } from '../interfaces/collaborators.interface'
 import { useAppDispatch, useAppSelector } from '../store/store'
 import { changeOrganizations } from '../store/organizations'
 import { changeCurrentOrg } from '../store/currentOrganizations'
 
 export default function useCollaborators(orgId: string | null) {
 	const [collaborators, setcollaborators] = useState<Collaborator[]>([])
-	const {data : Organizations, status} = useAppSelector(store => store.organizations)
+	const { data: Organizations, status } = useAppSelector(
+		store => store.organizations
+	)
 	const dispatch = useAppDispatch()
 	const [loading, setLoading] = useState<boolean>(true)
-	const [loadRol, setLoadRol] = useState<string>("")
-	
-	useEffect(() => {getCollaborators()}, [])
+	const [loadRol, setLoadRol] = useState<string>('')
+
+	useEffect(() => {
+		getCollaborators()
+	}, [])
 
 	const getCollaborators = async () => {
 		const { data } =
-			await CollaboratorServices.getCollaboratorsByOrganizationId(orgId || "")
+			await CollaboratorServices.getCollaboratorsByOrganizationId(orgId || '')
 		setcollaborators(data)
-        setLoading(false)
+		setLoading(false)
 	}
-	const updateCollaborator = async (id: string,collaborator: Collaborator, myUser : boolean) => {
+	const updateCollaborator = async (
+		id: string,
+		collaborator: Collaborator,
+		myUser: boolean
+	) => {
 		setLoadRol(id)
-		const {data} = await CollaboratorServices.update(id,{rol: collaborator.rol})
-		if(data){
-			const newColla = collaborators.filter(collaborator => collaborator.id !== id)
-			if(myUser){
-			const newStateOrganization = Organizations.filter(org => org.Organization.id !== collaborator.Organization.id)
-			dispatch(changeOrganizations([...newStateOrganization,collaborator]))
-			dispatch(changeCurrentOrg({...collaborator.Organization, rol: collaborator.rol}))
+		const { data } = await CollaboratorServices.update(id, {
+			rol: collaborator.rol,
+		})
+		if (data) {
+			const newColla = collaborators.filter(
+				collaborator => collaborator.id !== id
+			)
+			if (myUser) {
+				const newStateOrganization = Organizations.filter(
+					org => org.Organization.id !== collaborator.Organization.id
+				)
+				dispatch(changeOrganizations([...newStateOrganization, collaborator]))
+				dispatch(
+					changeCurrentOrg({
+						...collaborator.Organization,
+						rol: collaborator.rol,
+					})
+				)
 			}
-			setcollaborators([...newColla,collaborator])
+			setcollaborators([...newColla, collaborator])
 		}
-		setLoadRol("")
+		setLoadRol('')
 	}
-		
+
 	return {
 		collaborators,
 		getCollaborators,
 		updateCollaborator,
-        loading,
-		loadRol
+		loading,
+		loadRol,
 	}
 }
